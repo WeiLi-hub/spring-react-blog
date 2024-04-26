0448ef3dd7568dfa84d1c5d794877a2a628cd9b2
Weiqiang Li <lwq617597704@gmail.com>
2024-04-25 21:36:09 -0400

    Adding AuthService for user authentication

--- backend/src/main/java/com/blog/backend/Service/AuthService.java
+++ backend/src/main/java/com/blog/backend/Service/AuthService.java
@@ -1,6 +1,17 @@
 package com.blog.backend.Service;
 
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +19,9 @@ import com.blog.backend.Entity.BlogUser;
 import com.blog.backend.Repository.BlogUserRepository;
 import com.blog.backend.Security.JwtProvider;
 import com.blog.backend.dto.AuthRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Service
 public class AuthService {
@@ -46,6 +60,46 @@ public class AuthService {
                     return true;
                 });
     }
-
     
+    public ResponseEntity<JsonNode> login(AuthRequest loginRequest){
+        Authentication authenticate;
+        try{
+            authenticate = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUserName(), loginRequest.getPassword())
+            );
+            SecurityContextHolder.getContext().setAuthentication(authenticate);
+        }catch(AuthenticationException e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(badCredentialsResponseNode(loginRequest));
+        }
+
+        return ResponseEntity.ok(loginSuccessResponseNode(authenticate));
+    }
+
+    private ObjectNode badCredentialsResponseNode(AuthRequest loginRequest){
+        return new ObjectMapper().createObjectNode().put("error", badCredentialsMessage(loginRequest.getUserName()));
+    }
+
+    private String badCredentialsMessage(String username){
+        return blogUserRepository.findByUserName(username)
+                .map(user -> "Invalid password").orElse("Invalid username");
+    }
+
+    private ObjectNode loginSuccessResponseNode(Authentication authentication){
+        return new ObjectMapper().createObjectNode()
+                .put("username", authentication.getName())
+                .put("authorities", authorities(authentication.getAuthorities()))
+                .put("jwt", jwtProvider.generateToken(authentication));
+    }
+
+    private String authorities(Collection<? extends GrantedAuthority> grantedAuthority){
+        return grantedAuthority.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+    }
+
+    public Optional<BlogUser> getBlogUser(){
+        String blogUserName = SecurityContextHolder.getContext().getAuthentication().getName();
+        return blogUserRepository.findByUserName(blogUserName);
+    }
+
 }
