16f05cc969f5f0e91c9c52b61e0ff3dfabe605d8
Weiqiang Li <lwq617597704@gmail.com>
2024-04-29 23:07:36 -0400

    Post controller

--- backend/src/main/java/com/blog/backend/Controller/PostController.java
+++ backend/src/main/java/com/blog/backend/Controller/PostController.java
@@ -3,9 +3,12 @@ package com.blog.backend.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,7 +63,24 @@ public class PostController {
     }
 
     @PostMapping(value = "/posts")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     public ResponseEntity<JsonNode> createPost(@RequestBody ContentRequest contentRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(postRepository.save(newPost(contentRequest)).asJson());
     }
+
+    @PutMapping(value = "/posts")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE-USER')")
+    public ResponseEntity<JsonNode> updatePost(@RequestBody ContentRequest contentRequest){
+        return postRepository.findById(contentRequest.getId())
+                .map(post -> post.updateContent(contentRequest.getContent()))
+                .map(post -> ResponseEntity.ok(postRepository.save(post).asJson()))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(value = "posts/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+    public ResponseEntity<HttpStatus> deletePost(@PathVariable Long id){
+        postRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    } 
 }
